f4e7cde4f41ae74914b18e6e720c9487d9167e56
Filip Fog <hanfil@outlook.com>
2022-11-19 21:15:54 +0100

    new config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
+echo "Fetching updates from github repo."
+git pull
 
+echo "Creating /srv/salt directory. It fails if it exists."
 mkdir /srv/salt
 
 echo "Importing cybersalt state files, under /srv/salt/cybersalt."
